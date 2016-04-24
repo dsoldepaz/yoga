9011e231874f588ed1dc1d8ab73b2cce40042fba
Diartch25 <diartch25@gmail.com>
2016-04-24 15:19:27 -0600

    Added Script for Google Maps API

--- js/main.js
+++ js/main.js
@@ -2,23 +2,29 @@ $.ajax({
     dataType: 'text',
     success: function(string) {
         var nav = $.parseJSON(string);
-		
-		function parseMenu(ul, menu) {				
-			for (var i=0;i<menu.length;i++) {
-				var li=$(ul).append('<li><a href="'+menu[i].link+'">'+menu[i].name+'</a></li>');
-				if (menu[i].sub!=null) {
-					var subul=$('<ul id="submenu_'+menu[i].link+'"></ul>');
-					$(li).append(subul);
-					parseMenu($(subul), menu[i].sub);
-				}
-			}				
-		}
-		var menu=$('#menu');
-		menu.append('<ul id="menu-root">');
-		var ul=$('#menu-root')
-		parseMenu(ul, nav.menu);     
+        
+        function parseMenu(ul, menu) {              
+            for (var i=0;i<menu.length;i++) {
+                var li=$(ul).append('<li><a href="'+menu[i].link+'">'+menu[i].name+'</a></li>');
+                if (menu[i].sub!=null) {
+                    var subul=$('<ul id="submenu_'+menu[i].link+'"></ul>');
+                    $(li).append(subul);
+                    parseMenu($(subul), menu[i].sub);
+                }
+            }               
+        }
+        var menu=$('#menu');
+        menu.append('<ul id="menu-root">');
+        var ul=$('#menu-root')
+        parseMenu(ul, nav.menu);     
     },
     url: 'navigation.json'
 });
 
-
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
