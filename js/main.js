3659052fce312c566900a798cf25fa4cd30443b8
David <d.soldepaz@gmail.com>
2016-04-24 12:17:25 -0600

    Menu creation revised and style linked

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-var nav;
-
 $.ajax({
     dataType: 'text',
     success: function(string) {
-        nav = $.parseJSON(string);		
+        var nav = $.parseJSON(string);
+		
+		function parseMenu(ul, menu) {				
+			for (var i=0;i<menu.length;i++) {
+				var li=$(ul).append('<li><a href="'+menu[i].link+'">'+menu[i].name+'</a></li>');
+				if (menu[i].sub!=null) {
+					var subul=$('<ul id="submenu_'+menu[i].link+'"></ul>');
+					$(li).append(subul);
+					parseMenu($(subul), menu[i].sub);
+				}
+			}				
+		}
+		var menu=$('#menu');
+		menu.append('<ul id="menu-root">');
+		var ul=$('#menu-root')
+		parseMenu(ul, nav.menu);     
     },
     url: 'navigation.json'
 });
 
-$(function () {      
-    function parseMenu(ul, menu) {
-		for (var i=0;i<menu.length;i++) {
-			var li=$(ul).append('<li><a href="'+menu[i].link+'">'+menu[i].name+'</a></li>');
-			if (menu[i].sub!=null) {
-				var subul=$('<ul id="submenu'+menu[i].link+'"></ul>');
-				$(li).append(subul);
-				parseMenu($(subul), menu[i].sub);
-			}
-		}
-	}
-	var menu=$('#menu');
-	parseMenu(menu, nav.menu);
-});
+
